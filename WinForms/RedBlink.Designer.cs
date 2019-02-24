54628369723cfba109c1d79a9271d0d0f8b3572c
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2019-02-24 21:47:50 +0100

    initial commit

---
+++ WinForms/RedBlink.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace RedBlinkWinForms
+{
+    partial class RedBlink
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RedBlink));
+            this.lbl_RedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_RedLabel
+            // 
+            resources.ApplyResources(this.lbl_RedLabel, "lbl_RedLabel");
+            this.lbl_RedLabel.BackColor = System.Drawing.Color.Red;
+            this.lbl_RedLabel.Name = "lbl_RedLabel";
+            this.lbl_RedLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Lbl_RedLabel_MouseDown);
+            // 
+            // RedBlink
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbl_RedLabel);
+            this.Name = "RedBlink";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_RedLabel;
+    }
+}
+
---
+++ WinForms/RedBlink.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Timer = System.Windows.Forms.Timer;
+
+namespace RedBlinkWinForms
+{
+    public partial class RedBlink : Form
+    {
+        private Task _TimeController;
+        private CancellationTokenSource _CancellationTokenSource;
+
+        private Timer _DoubleClickTimer = new Timer();
+        private bool _IsFirstClick = true;
+        private bool _IsDoubleClick = false;
+        private int _Milliseconds = 0;
+
+        public RedBlink()
+        {
+            InitializeComponent();
+            InitControls();
+            InitComponents();
+        }
+
+        private void InitComponents()
+        {
+            _CancellationTokenSource = new CancellationTokenSource();
+
+            _TimeController = new Task(async (token) =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        await Task.Delay(1000);
+                        this.Invoke((MethodInvoker)delegate { Visible = true; });
+                    }
+                }
+                catch { }
+            }, _CancellationTokenSource.Token);
+
+            _TimeController.Start();
+
+
+            _DoubleClickTimer.Interval = 100;
+            _DoubleClickTimer.Tick +=
+                new EventHandler(DoubleClickTimer_Tick);
+        }
+
+        private void InitControls()
+        {
+            this.Size = new Size(600, 400);
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            this.lbl_RedLabel.BackColor = Color.Red;
+        }
+
+        void DoubleClickTimer_Tick(object sender, EventArgs e)
+        {
+            _Milliseconds += 100;
+
+            if (_Milliseconds >= SystemInformation.DoubleClickTime)
+            {
+                _DoubleClickTimer.Stop();
+
+                if (_IsDoubleClick)
+                {
+                    _CancellationTokenSource.Cancel();
+                    _TimeController.Wait();
+                    Application.Exit();
+                }
+                else
+                {
+                    this.Visible = false;
+                }
+
+                _IsFirstClick = true;
+                _IsDoubleClick = false;
+                _Milliseconds = 0;
+            }
+        }
+
+        private void Lbl_RedLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (_IsFirstClick)
+            {
+                _IsFirstClick = false;
+                _DoubleClickTimer.Start();
+            }
+            else
+            {
+                if (_Milliseconds < SystemInformation.DoubleClickTime)
+                {
+                    _IsDoubleClick = true;
+                }
+            }
+        }
+    }
+}
