c2a4765c768b3cab904e6eacbdf1b7bd612e4667
jkaneneks <34597495+jkaneneks@users.noreply.github.com>
2019-02-25 16:38:47 +0100

    Add files via upload

---
+++ WinForms/Messages.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace RedBlinkWinForms
+{
+    public static class Messages
+    {
+        private static string[] _Excercises;
+
+        public static string[] Excercises
+        {
+            get
+            {
+                if (_Excercises == null)
+                {
+                    _Excercises = new string[]
+                    {
+                        "Wiederkauen",
+                        "Augentraining",
+                        "Schulterkreisen",
+                        "Wirbelsäule entblocken",
+                        "Weitsehen",
+                        "Atlas gleiten",
+                        "Blickfang",
+                        "Hände ausschütteln",
+                        "Spaziergang"
+                    };
+                }
+
+                return _Excercises;                
+            }
+        }
+
+        public static int MessageCount => Excercises.Length;
+    }
+}
